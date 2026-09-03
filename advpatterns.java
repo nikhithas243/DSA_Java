68358a7d6bf7cb53bb49e6c42ed2900193a326d7
Nikhitha <143338698+nikhithas243@users.noreply.github.com>
2026-09-03 23:02:58 +0530

    Update advpatterns.java

--- advpatterns.java
+++ advpatterns.java
@@ -132,6 +132,33 @@ public static void diamond(int n){
     }
 
 }
+//2 EXTRA ASSIGNMENT QUESTIONS FROM THE PDF IN PATTERNS MODULE
+public static void number_pyramid(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print(i+" ");
+        }
+       System.out.println();
+    }
+}
+
+public static void palindrome_pyramid_Nos(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=i;j>=1;j--){
+            System.out.print(j);    
+        }
+        for(int j=2;j<=i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int row=sc.nextInt();
@@ -146,6 +173,9 @@ public static void diamond(int n){
     solid_rhombus(row);
     hollow_rhombus(row);
     diamond(row);
+    //2 EXTRA ASSIGNMENT QUESTIONS FROM THE PDF IN PATTERNS MODULE
+    number_pyramid(row);
+    palindrome_pyramid_Nos(col);
 }
     
     
