e967e4c16874ded26f4e1a71a47b000546ccf7f9
Nikhitha <143338698+nikhithas243@users.noreply.github.com>
2026-09-05 22:57:27 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,10 @@
+//Arrays=> It is the data structure that contains similar data type and is contingous data type. The array is stored in continous memory locations.
+//Array-indexing. Array's location start with 0 index to n-1. It is used to give index and access elements faster
+//Create: int arr[]=new int[50];
+//Create:int arr[]={"Mango","Apple","pomogranate"};
+//Create: int arr[]={24,35,68};
+//Input: int arr[0]=sc.nextInt(); 
+//Output
+//Create 
+//Update
+//Linear Search
