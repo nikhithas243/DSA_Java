75e5f4fcc7e4c4cb3f073486f255dad81318ffa7
Nikhitha <143338698+nikhithas243@users.noreply.github.com>
2026-09-08 11:42:11 +0530

    Add files via upload

---
+++ arrayqs.java
@@ -0,0 +1,96 @@
+import java.util.*;
+public class arrayqs {
+    public static int linearsearch(int num[],int key){
+        for(int i=0;i<num.length;i++){
+            if(num[i]==key)return i;
+        }
+        return -1;
+    }
+    public static int menu(String num[],String key){
+        for(int i=0;i<num.length;i++){
+            if(num[i].equals(key)) return i; //== doesn't work for strings it is .equals() operator
+        }
+        return -1;
+    }
+    public static int largest(int num[]){
+        int large=Integer.MIN_VALUE;
+        for(int i=0;i<num.length;i++){
+            if(num[i]>large) large=num[i];
+           // large=Math.max(large,num[i]);
+        }
+        return large;
+    }
+    public static int smallest(int num[]){
+        int small=Integer.MAX_VALUE;
+        for(int i=0;i<num.length;i++){
+            if(num[i]<small) small=num[i];
+           // large=Math.max(large,num[i]);
+        }
+        return small;
+    }
+    public static int binarySearch(int num[],int key){
+        int start=0;
+        int end=num.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(num[mid]==key){
+                return mid;
+            }else if(num[mid]<key){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+        
+    }
+    
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num[]=new int[5];
+        System.out.println("Enter the array");
+        for(int i=0;i<num.length;i++){
+            num[i]=sc.nextInt();
+        }
+
+        // //LINEAR SEARCH
+        // System.out.println("Linear search: Enter key to find");
+        // int key=sc.nextInt();
+        // if(linearsearch(num, key)==-1){
+        //     System.out.println("Key is not found");
+        // }else{
+        //      System.out.println("Key is at index "+linearsearch(num, key));
+        // }
+
+        //LINEAR SEARCH ASSIGNMENT-MENU of Strings
+        // String breakfast[]=new String[5];
+        // System.out.println("Enter the array");
+        // for(int i=0;i<breakfast.length;i++){
+        //     breakfast[i]=sc.next();
+        // }
+        // System.out.println("Linear search: Enter key to find");
+        // String key=sc.next();
+        // if(menu(breakfast, key)==-1){
+        //     System.out.println("Key is not found");
+        // }else{
+        //     System.out.println("Key is at index "+menu(breakfast, key));
+        // }
+       
+        //LARGEST IN ARRAY
+        System.out.println("The largest in array "+largest(num));
+
+        //SMALLEST IN ARRAY
+        System.out.println("The smallest in array "+smallest(num));
+
+        //BINARY SEARCH
+        System.out.println("Enter key to search");
+        int key=sc.nextInt();
+        if(binarySearch(num,key)==-1){
+        System.out.println("Element not found");
+        }else{
+        System.out.println("The index where the element is found is "+binarySearch(num,key));
+       }
+
+    }
+}
